44ee43461171668ecc161300e911c92d63c726ae
Kotenko Dmytro <kotenko2002@gmail.com>
2023-11-18 14:03:08 +0200

    setup project structure

---
+++ RentAPI/Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+    }
+}
