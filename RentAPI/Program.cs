01602285d7c76dc7faf58a35063fc38dd7800b71
Kotenko Dmytro <kotenko2002@gmail.com>
2023-11-18 13:52:49 +0200

    delete trash

--- RentAPI/Program.cs
+++ RentAPI/Program.cs
@@ -9,13 +9,11 @@ namespace RentAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
